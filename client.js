dcea23baeadc777430551fe8463c7fac7aee8ceb
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-12 20:43:12 +0000

    handle ctrl+c

--- client.js
+++ client.js
@@ -8,12 +8,34 @@ const connect = function() {
 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('connect', () => {console.log('Successfully connected to game server.')
-                            conn.write('Name: HIW')});
+  conn.on('connect', () => {console.log('Successfully connected to game server.');
+                            conn.write('Name: HIW');
+                            setTimeout(() => conn.write('Move: up'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: up'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: right'), 50);
+                            setTimeout(() => conn.write('Move: up'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: right'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: right'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: down'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);
+                            setTimeout(() => conn.write('Move: left'), 50);});
 
   conn.on('data', () => console.log('you ded cuz you idled'));
 
   return conn;
 }
+
+
 
 module.exports = { connect };
