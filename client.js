01d3bf6401ad5d267de5fd5aec597a9263988b49
Hsing-I Wang <hsing.sophie@gmail.com>
2021-01-12 19:19:50 +0000

    print out the name

--- client.js
+++ client.js
@@ -3,11 +3,15 @@ const net = require('net');
 const connect = function() {
   const conn = net.createConnection({ 
     host: '10.0.2.15',
-    port: 50541
+    port: 50541,
   });
+
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('data', () => console.log("you ded cuz you idled"));
+  conn.on('connect', () => {console.log('Successfully connected to game server.')
+                            conn.write('Name: HIW')});
+
+  conn.on('data', () => console.log('you ded cuz you idled'));
 
   return conn;
 }
